de8c8260a1de48638e65b3e24f62264a4f80e708
rbs517 <rebeccaskurnik@gmail.com>
2017-07-27 16:58:06 -0400

    new js

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ And then populate the result on the page using jQuery
 */
 var app = {
 	//Define the url for the wikipedia API call
-	musixMatchURL: "http://api.musixmatch.com/ws/1.1/track.search?q_track=",
-	//currentWord: "salad",
-	musixMatchRest:"&page_size=10&page=1&s_track_rating=desc",
+	musixMatchURL: "http://api.musixmatch.com/ws/1.1/track.search?&format=jsonp&q_track=",
+	//newCurrentWord: "salad",
+	//musixMatchRest:"&page_size=10&page=1&s_track_rating=desc",
 	musixMatchKey:"&apikey=d919d8d046718bd5469b63e886f873cc",
 
-
 	//Define an intial search term
-	
 	//A place to set up listeners or kick off an initial function
 	initialize: function() {
 		$("#search").click(function() {
@@ -20,46 +18,31 @@ var app = {
   		$("#resultsTarget").html("");
   		//Use jQuery to get the value of the 'query' input box
 		var newCurrentWord = $("#query").val();
-		console.log(newCurrentWord);
+		//console.log(newCurrentWord);
 		//Execute the MusixMatch API call function with the currentWord var as the argument
 		app.searchMusixMatch(app.newCurrentWord);
-});
+	});	
 		//What if someone just wants to click "ENTER"?
 		//Use jQuery to assign a callback function when enter is pressed 
 		//This will ONLY work when the 'query' input box is active
 		$("#query").keypress(function(e){
-			//console.log(e);
+			console.log(e);
 			//If enter key is pressed
 			if (e.which == 13){
-				//Use jQuery's trigger() function to execute the click event
-				$("#search").trigger('click');
+			//Use jQuery's trigger() function to execute the click event
+			$("#search").trigger('click');
 			}
 		});
-	},
+	},	
 	//Define a function to execute the AJAX call
 	//The argument will be the desired search term
-	searchMusixMatch: function(searchTerm) {
-		console.log("Executing the searchMusixMatch function");
-		/*
-		Use jQuery's ajax method to get the data
+	searchMusixMatch: function(word){
+		console.log("Executing the searchMusixMatch function");	
 
-		The jQuery ajax method can accept an object as its argument
-			$.ajax(objectGoesHere)
-
-		In the example below, we define/pass-in an object that has 5 properties
-			- url
-			- type
-			- dataType
-			- error
-			- success
-		See the jQuery documentation for a full list of acceptable properties
-		http://api.jquery.com/jquery.ajax/
-
-		*/
 		$.ajax({
-			url: app.musixMatchURL+ searchTerm + app.musixMatchRest + app.musixMatchKey, //word is the argument passed into the function
+			url: app.musixMatchURL + word /*+ app.musixMatchRest+ &format=jsonp& */ + app.musixMatchKey, //word is the argument passed into the function
 			type: 'GET',
-			dataType: 'json',
+			dataType: 'jsonp',
 			error: function(data){
 				console.log("We got problems");
 				//console.log(data.status);
@@ -67,22 +50,25 @@ var app = {
 			success: function(data){
 				console.log("WooHoo!");
 				//Check the browser console to see the returned data
-				//console.log(data);
+				console.log(data);
 				//Use jQuery to insert the search term into the appropriate DOM element
-				$("#searchTerm").html(data[0]);
-				var searchResults = data[0].message.body.track_list[0].track_name;
-				console.log(searchResults);
+				//$("#searchTerm").html(data[0]);
+				var searchResults1 = (data.message.body.track_list[0].track_name); 
+				var searchResults2 = (data.message.body.track_list[0].artist_name);
+				console.log(searchResults1);
+				console.log(searchResults2);
 				//Loop through our array of results
-				for (var i = 0; i < searchResults.length; i++){
+				//for (var i = 0; i < searchResults.length; i++){
 					//Create an html string with a tag, class, and the search result
-					var htmlString = "<p class='mmResults'>" + searchResults[i] + "</p>";
-
+					var htmlString = "<p class='mmResults'>" + searchResults1 + searchResults2 + "</p>";
+				//var printResults = function(data){
+					//printResults(searchResults1 + searchResults2);
 					//Use jQuery's append() function to add the searchResults to the DOM
 					$("#resultsTarget").append(htmlString);
 
 				}
 			}
-		});
+		);
 	}
 
 };
